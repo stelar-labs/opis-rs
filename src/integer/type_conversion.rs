7ab50ea8b9dbd014217d1d7da4eb665067088da7
Roy Okello <0xR3y@protonmail.com>
2022-11-29 07:06:55 +0300

    v5.0.1
    improved performance for from<&u8> and into<vec<u8>> for integer

--- src/integer/type_conversion.rs
+++ src/integer/type_conversion.rs
@@ -27,13 +27,17 @@ impl From<Vec<u8>> for Integer{
 }
 
 impl From<&[u8]> for Integer{
+
     fn from(bytes: &[u8]) -> Self {
-        Integer::from_bin(
-            &bytes
-                .iter()
-                .fold(String::new(), |acc, x| format!("{}{:08b}", acc, x))
-        ).unwrap()
+
+        let p: Vec<String> = bytes.iter().map(|x| format!("{:08b}",x)).collect();
+        
+        let q = p.concat();
+
+        Integer::from_bin(&q).unwrap()
+        
     }
+
 }
 
 impl Into<Vec<u8>> for Integer{
@@ -50,97 +54,93 @@ impl Into<Vec<u8>> for &Integer{
 
 fn bits_to_bytes(bits: &[Bit]) -> Vec<u8> {
 
-    let r = bits.len() % 8;
-    
-    let (bytes, _) = bits
-        .iter()
-        .fold((vec![], vec![bits[0]; 8 - r]), |(mut bytes, mut byte), x| {
-            
-            byte.push(*x);
-            
-            if byte.len() == 8 {
+    let mut bytes = vec![];
 
-                let byte_string: String = byte
-                    .iter()
-                    .map(|y| {
-                        match y {
-                            Bit::One => '1',
-                            Bit::Zero => '0'
-                        }
-                    })
-                    .collect();
+    let r = 8 - (bits.len() % 8);
 
-                bytes.push(u8::from_str_radix(&byte_string, 2).unwrap());
+    let mut byte = vec![bits[0]; r];
 
-                byte.clear()
-                
-            }
+    bits
+    .iter()
+    .for_each(|&i| {
 
-            (bytes, byte)
-            
-        });
+        byte.push(i);
+
+        if byte.len() == 8 {
+
+            let p: Vec<String> = byte.iter().map(|x| format!("{:?}",x)).collect();
         
+            let q = p.concat();
+
+            bytes.push(u8::from_str_radix(&q, 2).unwrap());
+
+            byte.clear();
+
+        }
+
+    });
+
     bytes
 
 }
 
 impl From<&u8> for Integer{
     fn from(value: &u8) -> Self {
-        Integer::from(&value.to_le_bytes()[..])
+        Integer::from(&value.to_be_bytes()[..])
     }
 }
 
 impl Into<u8> for &Integer{
     fn into(self) -> u8 {
-        u8::from_le_bytes(self.to_ext_bytes(1).try_into().unwrap())
+        u8::from_be_bytes(self.to_ext_bytes(1).try_into().unwrap())
     }
 }
 
 impl From<&u16> for Integer{
     fn from(value: &u16) -> Self {
-        Integer::from(&value.to_le_bytes()[..])
+        Integer::from(&value.to_be_bytes()[..])
     }
 }
 
 impl Into<u16> for &Integer{
     fn into(self) -> u16 {
-        u16::from_le_bytes(self.to_ext_bytes(2).try_into().unwrap())
+        u16::from_be_bytes(self.to_ext_bytes(2).try_into().unwrap())
     }
 }
 
 impl From<&u32> for Integer{
     fn from(value: &u32) -> Self {
-        Integer::from(&value.to_le_bytes()[..])
+        Integer::from(&value.to_be_bytes()[..])
     }
 }
 
 impl Into<u32> for &Integer{
     fn into(self) -> u32 {
-        u32::from_le_bytes(self.to_ext_bytes(4).try_into().unwrap())
+        u32::from_be_bytes(self.to_ext_bytes(4).try_into().unwrap())
     }
 }
 
 impl From<&u64> for Integer{
     fn from(value: &u64) -> Self {
-        Integer::from(&value.to_le_bytes()[..])
+        Integer::from(&value.to_be_bytes()[..])
     }
 }
 
 impl Into<u64> for &Integer{
     fn into(self) -> u64 {
-        u64::from_le_bytes(self.to_ext_bytes(8).try_into().unwrap())
+        u64::from_be_bytes(self.to_ext_bytes(8).try_into().unwrap())
     }
 }
 
 impl From<&u128> for Integer{
     fn from(value: &u128) -> Self {
-        Integer::from(&value.to_le_bytes()[..])
+        Integer::from(&value.to_be_bytes()[..])
     }
 }
 
 impl Into<u128> for &Integer{
     fn into(self) -> u128 {
-        u128::from_le_bytes(self.to_ext_bytes(16).try_into().unwrap())
+        u128::from_be_bytes(self.to_ext_bytes(16).try_into().unwrap())
     }
 }
 
@@ -152,6 +152,33 @@ impl From<&usize> for Integer{
 
 impl Into<usize> for &Integer{
     fn into(self) -> usize {
-        usize::from_le_bytes(self.to_ext_bytes((usize::BITS/8) as usize).try_into().unwrap())
+        usize::from_be_bytes(self.to_ext_bytes((usize::BITS/8) as usize).try_into().unwrap())
     }
+}
+
+#[cfg(test)]
+mod tests {
+    
+    use super::*;
+
+    #[test]
+    fn test_int_into_bytes() {
+
+        let int_bytes: Vec<u8> = Integer::three().into();
+
+        assert_eq!(int_bytes, vec![3]);
+
+    }
+
+    #[test]
+    fn test_large_int_into_bytes() {
+
+        let i = Integer::from_dec("1000").unwrap();
+
+        let i_bytes: Vec<u8> = i.into();
+
+        assert_eq!(i_bytes, vec![3, 232]);
+
+    }
+
 }
