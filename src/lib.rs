1dc1ed0948b5439cadce9fdc95552fa3fede3953
Roy <royokello@protonmail.com>
2024-06-03 14:33:14 +0300

    added digits to integer and add method

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
-mod bit;
-mod fraction;
+// mod bit;
+// mod fraction;
+// mod integer;
+// mod matrix;
+
+// #[derive(Copy, Clone, Hash)]
+// pub enum Bit { One, Zero }
+
+// #[derive(Clone, Hash)]
+// pub struct Integer(pub Vec<Bit>);
+
+// #[derive(Clone, Hash)]
+// pub struct Fraction(pub Integer, pub Integer);
+
+// #[derive(Clone)]
+// pub struct Matrix<T>(pub Vec<Vec<T>>);
+
 mod integer;
-mod matrix;
-
-#[derive(Copy, Clone, Hash)]
-pub enum Bit { One, Zero }
-
-#[derive(Clone, Hash)]
-pub struct Integer(pub Vec<Bit>);
-
-#[derive(Clone, Hash)]
-pub struct Fraction(pub Integer, pub Integer);
-
-#[derive(Clone)]
-pub struct Matrix<T>(pub Vec<Vec<T>>);
+pub use integer::Integer;
