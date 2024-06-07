87efe011701686e3ccae6f6876bf6a5f141870dc
Roy <royokello@protonmail.com>
2024-06-07 23:09:01 +0300

    added into bytes for &integer

--- src/integer/type_conversion.rs
+++ src/integer/type_conversion.rs
@@ -64,20 +64,20 @@ impl From<&[u8]> for Integer{
 
 impl Into<Vec<u8>> for Integer {
     fn into(self) -> Vec<u8> {
+        (&self).into()
+    }
+}
+
+impl Into<Vec<u8>> for &Integer{
+    fn into(self) -> Vec<u8> {
         let mut result = Vec::new();
-        for digit in self.digits {
+        for digit in &self.digits {
             let bytes = digit.to_le_bytes();
             result.extend_from_slice(&bytes);
         }
         result
     }
 }
-
-// impl Into<Vec<u8>> for &Integer{
-//     fn into(self) -> Vec<u8> {
-//         bits_to_bytes(&self.0)
-//     }
-// }
 
 // pub fn bits_to_bytes(bits: &[Bit]) -> Vec<u8> {
 
